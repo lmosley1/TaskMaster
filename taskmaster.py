472ecb6ce47efc60055a4ba5095fae17806fc2a4
Leo Mosley <leomosley12@gmail.com>
2022-07-22 10:22:40 +1000

    Update 22/07/22

--- taskmaster.py
+++ taskmaster.py
@@ -6,8 +6,8 @@ root = tkinter.Tk()
 root.title("TaskMaster")
 
 
-# Adds a task to the listbox from the entry widget
 def add_task():
+    """Adds a task to the listbox from the entry widget"""
     task = task_entry.get()
     if task != "":
         tasks_listbox.insert(tkinter.END, task)
@@ -16,34 +16,39 @@ def add_task():
         tkinter.messagebox.showwarning(title="Error", message="Enter a task first")
 
 
-# Deletes selected task
 def delete_task():
+    """Delets the selected task"""
     try:
         task_index = tasks_listbox.curselection()[0]
         tasks_listbox.delete(task_index)
-    except:
+    except IndexError:
         tkinter.messagebox.showwarning(title="Error", message="Select a task first")
 
 
-# Loads tasks that are saved in tasks.dat
-# Deletes previous tasks in the listbox so it doesn't load on top of them
 def load_tasks():
+    """Loads tasks that are saved in tasks.dat
+    Deletes previous tasks in the listbox so it doesn't load on top of them
+    """
     try:
         tasks = pickle.load(open("tasks.dat", "rb"))
         tasks_listbox.delete(0, tkinter.END)
         for task in tasks:
             tasks_listbox.insert(tkinter.END, task)
-    except:
+    except FileNotFoundError:
         tkinter.messagebox.showwarning(title="Error", message="Save a task list first")
 
 
-# Saves tasks to tasks.dat
 def save_tasks():
+    """Saves tasks to tasks.dat"""
     tasks = tasks_listbox.get(0, tasks_listbox.size())
     pickle.dump(tasks, open("tasks.dat", "wb"))
 
 
 # GUI
+
+title_text = tkinter.Label(root, text="TaskMaster")
+title_text.pack()
+title_text.config(font=("Helvetica", 30))
 
 # Makes a frame to put the listbox and scrollbar together
 tasks_frame = tkinter.Frame(root)
@@ -80,5 +85,11 @@ save_tasks_button = tkinter.Button(
 )
 save_tasks_button.pack()
 
+
+# Binds the Enter key to the add_task function
+root.bind("<Return>", (lambda event: add_task()))
+
+# Binds the Delete key to the delete_task function
+root.bind("<BackSpace>", (lambda event: delete_task()))
 
 root.mainloop()
