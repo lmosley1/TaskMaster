a068f5acbb2e4143f0059fca72c99cb90704e2ca
Leo Mosley <leomosley12@gmail.com>
2022-07-26 14:30:29 +1000

    Removes functions and updates taskmaster

--- taskmaster.py
+++ taskmaster.py
@@ -1,9 +1,12 @@
+"""module providing GUI for the software"""
 import tkinter
 import tkinter.messagebox
 import pickle
 
 root = tkinter.Tk()
 root.title("TaskMaster")
+# Makes the window unresizeable
+root.resizable(0, 0)
 
 
 def add_task():
@@ -48,13 +51,13 @@ def save_tasks():
 
 title_text = tkinter.Label(root, text="TaskMaster")
 title_text.pack()
-title_text.config(font=("Helvetica", 30))
+title_text.config(font=("Helvetica", 30), foreground="lime")
 
 # Makes a frame to put the listbox and scrollbar together
 tasks_frame = tkinter.Frame(root)
 tasks_frame.pack()
 
-tasks_listbox = tkinter.Listbox(tasks_frame, height=10, width=50)
+tasks_listbox = tkinter.Listbox(tasks_frame, height=20, width=50)
 tasks_listbox.pack(side=tkinter.LEFT)
 
 tasks_scrollbar = tkinter.Scrollbar(tasks_frame)
@@ -64,25 +67,49 @@ tasks_scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 tasks_listbox.config(yscrollcommand=tasks_scrollbar.set)
 tasks_scrollbar.config(command=tasks_listbox.yview)
 
+# Creates an entry box to input the task into
 task_entry = tkinter.Entry(root, width=50)
 task_entry.pack()
 
+
 # Adds the buttons to the GUI with their functions
-add_task_button = tkinter.Button(root, text="Add task", width=48, command=add_task)
+add_task_button = tkinter.Button(
+    root,
+    text="Add task",
+    highlightbackground="black",
+    fg="lime",
+    width=48,
+    command=add_task,
+)
 add_task_button.pack()
 
 delete_task_button = tkinter.Button(
-    root, text="Delete task", width=48, command=delete_task
+    root,
+    text="Delete task",
+    highlightbackground="black",
+    fg="lime",
+    width=48,
+    command=delete_task,
 )
 delete_task_button.pack()
 
 load_tasks_button = tkinter.Button(
-    root, text="Load tasks", width=48, command=load_tasks
+    root,
+    text="Load tasks",
+    highlightbackground="black",
+    fg="lime",
+    width=48,
+    command=load_tasks,
 )
 load_tasks_button.pack()
 
 save_tasks_button = tkinter.Button(
-    root, text="Save tasks", width=48, command=save_tasks
+    root,
+    text="Save tasks",
+    highlightbackground="black",
+    fg="lime",
+    width=48,
+    command=save_tasks,
 )
 save_tasks_button.pack()
 
@@ -90,7 +117,7 @@ save_tasks_button.pack()
 # Binds the Enter key to the add_task function
 root.bind("<Return>", (lambda event: add_task()))
 
-# Binds the Delete key to the delete_task function
+# Binds the Delete/Backspace key to the delete_task function
 root.bind("<BackSpace>", (lambda event: delete_task()))
 
 root.mainloop()
